e002dc2b9b7b5d0fb33d54b7554ca7e1665409b1
alessiorama <alessio.rama@skywarder.eu>
2026-05-18 00:06:36 +0200

    Immagini subcruise

--- Subcruise_parameters.m
+++ Subcruise_parameters.m
@@ -83,7 +83,7 @@ TSFC=fmat./I_sp_a;
 
 for i=1:size(I_sp_a, 1)
     for j=1:size(I_sp_a, 2)
-        if I_sp_a(i, j)<=0 || TSFC(i, j)<=0 || TSFC(i, j)>=1e-4 || eps_cool(i, j)>0.05
+        if I_sp_a(i, j)<=0 || TSFC(i, j)<=0 || TSFC(i, j)>=1e-4 %|| eps_cool(i, j)>0.05
             I_sp_a(i, j)=NaN;
             TSFC(i, j) = NaN;
         end
@@ -92,55 +92,30 @@ end
 
 maxValue = max(max(I_sp_a));
 [ind12, ind22] = find(I_sp_a==maxValue);
-
-% if ismember('plot', varargin)
-%     figure()
-%     t=surf(Ttot3, b, I_sp_a');
-%     view([1, 1, 0.25])
-%     t.EdgeColor='none';
-%     pbaspect([1, 1, 1])
-%     hold on;
-%     plot3(f(ind12), b(ind22), I_sp_a(ind12, ind22),'o', 'Color', 'r', 'MarkerFaceColor','r')
-%     x=t.XData;
-%     y=t.YData;
-%     z=t.ZData;
-%     %%Create vectors out of surface's XData and YData
-%     x=x(:,1);
-%     y=y(1,:);
-%     %%Divide the lengths by the number of lines needed
-%     xnumlines = 50; % 10 lines
-%     ynumlines =50; % 10 partitions
-%     xspacing = round(length(x)/xnumlines);
-%     yspacing = round(length(y)/ynumlines);
-%     %%Plot the mesh lines 
-%     % Plotting lines in the X-Z plane
-%     hold on
-%     for i = 1:yspacing:length(y)
-%         Y1 = y(i)*ones(size(x)); % a constant vector
-%         Z1 = z(i,:);
-%         plot3(x,Y1,Z1,'-k');
-%     end
-%     % Plotting lines in the Y-Z plane
-%     for i = 1:xspacing:length(x)
-%         X2 = x(i)*ones(size(y)); % a constant vector
-%         Z2 = z(:,i);
-%         plot3(X2,y,Z2,'-k');
-%     end
-% 
-%     figure()
-%     t=surf(f, b, TSFC');
-%     view([1, 1, 1])
-%     t.EdgeColor='none';
-%     pbaspect([1, 1, 1])
-%     hold on;
-%     plot3(f(ind12), b(ind22),TSFC(ind12, ind22),'o', 'Color', 'r', 'MarkerFaceColor','r')
-% 
-%     % TSFC diminuisce con beta; per ogni beta ha un ottimo su f
-%     % I_sp_a ha un ottimo a un certo beta e un certo f
-% 
-% 
-% end
-
+if ismember('plot', varargin)
+    figure()
+    contourf(Ttot3, b, TSFC'*3600*1000, 10, "FaceAlpha",0.75);
+    xlabel("$T_{tot, 4}\,[K]$", 'Interpreter','latex')
+    c = colorbar;
+    c.Label.String = "$TSFC\, \left[\, \frac{Kg}{kN\,h} \,\right]$";
+    c.Label.Interpreter = 'latex';
+    ylabel("$\beta_c$", 'Interpreter','latex')
+    figure()
+    bImax=zeros(size(Ttot3));
+    for temp=1:length(Ttot3)
+        [Tmaxb, idmax] = max(I_sp_a(temp, :));
+        bImax(temp) = b(idmax);
+    end
+    contourf(Ttot3, b, I_sp_a',11,"FaceAlpha",0.75);
+     xlabel("$T_{tot, 4}\,[K]$", 'Interpreter','latex')
+    c = colorbar;
+    c.Label.String = "$I_{sp,a}\,[s]$";
+    c.Label.Interpreter = 'latex';
+    ylabel("$\beta_c$", 'Interpreter','latex')
+    hold on
+    plot(Ttot3, bImax, '--r', 'LineWidth', 1.5)
+    legend("","$\max_{\beta_c}[I_{sp,a}]$", 'Interpreter','latex')
+end
 subCruise.m_a = T_subsonic/I_sp_a(ind12, ind22);
 subCruise.f=fmat(ind12, ind22);
 subCruise.b=bmat(ind12, ind22);
