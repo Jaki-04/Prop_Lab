2f320e688f816e0abf1edf9f484803ac841058b5
alessiorama <alessio.rama@skywarder.eu>
2026-08-01 17:11:26 +0200

    fix

--- Turbina.m
+++ Turbina.m
@@ -1,4 +1,4 @@
-function [P_t5, T_t5cool, M5, A_Out_Comp, L_turbina] = Turbina()
+function [p_5, T_5cool, M5, A5] = Turbina()
 
 subCruise = Subcruise_parameters();
 Air = Air_parameters('12000');
@@ -108,15 +108,15 @@ T_t2=247.9559;
 Pc = m_a*cp_a*(T_t3-T_t2);
 Pt = Pc / eta_m;
 
-T_t5cool = ((1 - eps_cool +f) * cp_GC * T_t4 + eps_cool * cp_a * T_tiii - Pt/ m_a) / ((1 - eps_cool) * cp_GC + eps_cool * cp_a);
+T_t5cool = ((1 - eps_cool +f) * cp_GC * T_t4 + eps_cool * cp_a * T_t3 - Pt/ m_a) / ((1 - eps_cool) * cp_GC + eps_cool * cp_a);
 taut = T_t5cool / T_t4;
 pi_t = taut ^ (g_GC / ((g_GC - 1) * e_t));
 
-T5_cool = T_t5cool - (0.5 * Cz_ii^2) / cp_GC;
-M5 = Cz_ii / sqrt(g_GC * R_GC * T5_cool);
-P_t5 = pi_t * P_t4;
-P5 = P_t5 / (1 + (M5^2) * (g_GC - 1) / 2);
-rho5 = P5 / (R_GC * T5_cool);
+T_5cool = T_t5cool - (0.5 * Cz_ii^2) / cp_GC;
+M5 = Cz_ii / sqrt(g_GC * R_GC * T_5cool);
+p_t5 = pi_t * P_t4;
+p_5 = p_t5 / (1 + (M5^2) * (g_GC - 1) / 2);
+rho5 = p_5 / (R_GC * T_5cool);
 A5 = m_0 / (rho5 * Cz_ii);
 
 r_h_exit = sqrt(r_t^2-A5/pi);
